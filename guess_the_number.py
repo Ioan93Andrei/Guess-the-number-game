8f11909e3133fcf825d75f5a7da43c74b0398ff2
Ioan Ojica <ivan.andrei93@mail.ru>
2020-11-07 19:35:08 +0100

    bugs 1 and 2 from the .txt file solved

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,23 +1,24 @@
 import random
 
 random_number = random.randrange(0, 500)
-chosen_number = int(float(input("Please pick a number: ")))
+chosen_number = int(input("Please pick a number: "))
 
-while chosen_number != random_number:
-    print(chosen_number)
-
-    if chosen_number > 500:
-        print ("Number must be below 500.")
-        break
-    else:
-      if chosen_number > random_number:
+while True:
+    if chosen_number == 500 or chosen_number <= 0 :
+        print("Number must be below 500 and above 0.")
+        print(random_number)
+        chosen_number = int(input("Please pick a number: "))
+        continue
+    if chosen_number > random_number:
           print("Too high")
-          chosen_number = int(float(input("Please pick a number: ")))
-      elif chosen_number < random_number: 
+          chosen_number = int(input("Please pick a number: "))
+    elif chosen_number < random_number: 
           print("Too low")
-          chosen_number = int(float(input("Please pick a number: ")))
-          print ("Congratulations, you guessed right. The number was " + str(chosen_number))
-          break
+          chosen_number = int(input("Please pick a number: "))
+    else:
+        print("Congratulations, you guessed right. The number was " + str(chosen_number) + ".")
+        break
 
-print("Goodbye")
+
+
   
