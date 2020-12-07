6b833cd556349fb70b6929089cd4d6929fa36abd
Ioan-Andrei Ojica <ivan.andrei93@mail.ru>
2020-12-07 05:09:43 +0100

    game now restarts after user is prompted

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,24 +1,29 @@
 import random
 
-random_number = random.randrange(0, 500)
-chosen_number = int(input("Please pick a number: "))
+def gameOn():
+    random_number = random.randrange(0, 500)
+    chosen_number = int(input("Please pick a number: "))
+    while True:
+        if chosen_number == 500 or chosen_number <= 0 :
+            print("Number must be below 500 and above 0.")
+            print(random_number)
+            chosen_number = int(input("Please pick a number: "))
+            continue
+        if chosen_number > random_number:
+            print("Too high")
+            chosen_number = int(input("Please pick a number: "))
+        elif chosen_number < random_number: 
+            print("Too low")
+            chosen_number = int(input("Please pick a number: "))
+        else:
+            print("Congratulations, you guessed right. The number was " + str(chosen_number) + ".")
+            answer = input("Do you want to play again? Y/N ")
 
-while True:
-    if chosen_number == 500 or chosen_number <= 0 :
-        print("Number must be below 500 and above 0.")
-        print(random_number)
-        chosen_number = int(input("Please pick a number: "))
-        continue
-    if chosen_number > random_number:
-          print("Too high")
-          chosen_number = int(input("Please pick a number: "))
-    elif chosen_number < random_number: 
-          print("Too low")
-          chosen_number = int(input("Please pick a number: "))
-    else:
-        print("Congratulations, you guessed right. The number was " + str(chosen_number) + ".")
-        break
+            if answer in("Y", "y", 'yes'):
+                print("Restarting game!")
+                gameOn()
+            else:
+                print("Goodbye")
+            break
 
-
-
-  
+gameOn()
